7ac6c023fb2f280b5056fd4d9aec9ad4b05e9b34
Yantao <yantao@email.com>
2025-09-27 12:06:27 +0700

    add class

--- primitiveCreatorUi.py
+++ primitiveCreatorUi.py
@@ -1,2 +1,27 @@
-from Pyside6 import QtCore, QtGui, QtWidgets
-from shiboken6
+try:
+	from Pyside6 import QtCore, QtGui, QtWidgets
+	from shiboken6 import wrapInstance
+except:
+	from Pyside2 import QtCore, QtGui, QtWidgets
+	from shiboken2 import wrapInstance
+
+
+import maya.OpenMayaUI as omui
+
+class PrimitiveCreatorDialog(QtWidgets.QDialog):
+	def __init__(self, parent=None):
+		super().__init__(parent)
+
+		self.resize(300,300)
+		self.setWindowTitle('Primitive Creator')
+
+def run():
+	global ui
+
+	try:
+		ui.close()
+	except:
+		pass
+	ptr = wrapInstance(int(omui.MQtUtil.mainwindow()), QtWidgets.QWidget)
+	ui = PrimitiveCreatorDialog(parent=ptr)
+	ui.show() 
